e36ce9fe13273b181c5175fe44529a13c1fda5a3
singh_pk <praneed.kla@northbkk.ac.th>
2021-12-09 17:13:02 +0700

    react fragments

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ function App() {
     }
 
     return (
-        <div>
+        <React.Fragment>
             <AddUser onAddUser={onAddUserHandler}/>
             <UserList users={usersList}/>
-        </div>
+        </React.Fragment>
     );
 }
 
