9dd37d5571da6b02066a2d78efc2966bc76fbd3a
singh_pk <praneed.kla@northbkk.ac.th>
2021-12-09 16:07:41 +0700

    adding validation & resetting logic

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -13,18 +13,25 @@ const AddUser = (props) => {
     const addUserHandler = (event) => {
         event.preventDefault();
 
+        if (username.trim().length === 0 || age.trim().length === 0) {
+            return;
+        }
+        if (age < 1) {
+            return;
+        }
+
         console.log(username, age);
-        // setUsername('');
-        // setAge('');
+        setUsername('');
+        setAge('');
     }
 
     return (
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor="username">Username</label>
-                <input id="username" type="text" onChange={usernameChangeHandler}/>
+                <input id="username" type="text" value={username} onChange={usernameChangeHandler}/>
                 <label htmlFor="age">Age (Years)</label>
-                <input id="age" type="number" onChange={ageChangeHandler}/>
+                <input id="age" type="number" value={age} onChange={ageChangeHandler}/>
                 <Button type="submit">Add User</Button>
             </form>
         </Card>
